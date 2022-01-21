f6a0d37e71a1942d647e3564029473c19446f06f
turchimatteo <turchi12@gmail.com>
2022-01-21 15:57:47 +0100

    inizio e primi due esercizi

--- index.php
+++ index.php
@@ -7,6 +7,24 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <!-- Creare una variabile con un paragrafo di testo a vostra scelta. -->
+    <?php 
+    $presentazione = 'Matteo, vengo da Roma e sto iniziando ad usare PHP!';
+    ?>
+    <p>Ciao, mi chiamo <?php echo $presentazione; ?></p>
+    <!-- Stampare a schermo il paragrafo e la sua lunghezza. -->
+    <p>
+        <?php
+        $paragrafo = strlen($presentazione);
+        echo 'Lunghezza paragrafo: ' . $paragrafo;
+        ?>
+    </p>
+    <!-- Una parola da censurare viene passata dall'utente tramite parametro GET. -->
+    <p>
+        <?php
+        $censura = $_GET['censura'];
+        ?>
+        <p>Ciao, io mi chiamo <?php echo $_GET['***']; ?></p>
+    </p>
 </body>
 </html>
